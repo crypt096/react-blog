ddd37ff8334bf4b6165677959fb29e291b86f18b
crypt0 <acvjetan@gmail.com>
2020-08-19 18:42:12 +0200

    Add GET and DELETE functionality.

--- src/components/BlogItems/BlogItems.js
+++ src/components/BlogItems/BlogItems.js
@@ -1,63 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import moment from "moment";
 
 import "./blog-items.scss";
 
 function BlogItems() {
+  const [blogItems, setBlogItems] = useState([]);
+
+  useEffect(() => {
+    loadBlogItems();
+  }, []);
+
+  const loadBlogItems = async () => {
+    const res = await axios.get(
+      "https://frontend-api-test-nultien.azurewebsites.net/api/BlogPosts"
+    );
+    setBlogItems(res.data.resultData);
+  };
+
+  const deletePost = async (id) => {
+    await axios.delete(
+      `https://frontend-api-test-nultien.azurewebsites.net/api/BlogPosts/${id}`
+    );
+    loadBlogItems();
+  };
+
   return (
-    <div className="blog-items-wrapper">
-      <div className="row no-gutters">
-        <div class="image-wrapper">
-          <img
-            className="blog-item-avatar"
-            src="https://i.picsum.photos/id/454/80/80.jpg?hmac=lHlKkUsxK0B6KymVYwr7iMd-VQeoP4XvY0IQKEJ4zXY"
-            alt="Blog img"
-          />
-        </div>
-        <div class="blog-item-info-wrapper">
-          <h5 className="blog-item-title">Blog post 1</h5>
-          <p className="blog-item-details">Posted by blablah on 29.09.2020</p>
-        </div>
-        <div className="blog-item-btn-wrapper">
-          <button type="button" className="blog-item-btn edit-btn">
-            Edit
-          </button>
-          <button type="button" className="blog-item-btn delete-btn">
-            Delete
-          </button>
-        </div>
-      </div>
-      <div className="row no-gutters">
-        <p className="blog-post-content">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-      </div>
-      <div className="row no-gutters">
-        <div className="blog-post-images-wrapper">
-          <img
-            src="https://i.picsum.photos/id/109/100/100.jpg?hmac=LeINYGZbnzwgFSznadxHjdAq1Mi13ARh3urcQf0H_LA"
-            alt=""
-            className="blog-post-image"
-          />
-          <img
-            src="https://i.picsum.photos/id/195/100/100.jpg?hmac=vRu_NpzSS_L4oZCg-OfhAeRweDc_RHC5UccZxQ6rTLk"
-            alt=""
-            className="blog-post-image"
-          />
-          <img
-            src="https://i.picsum.photos/id/140/100/100.jpg?hmac=4Skh7lTq0bdRmnXi_Pkq6odpzJruyj3UDBQWGHkCO-o"
-            alt=""
-            className="blog-post-image"
-          />
-        </div>
-      </div>
+    <div>
+      {blogItems.map((item, index) => {
+        const postedTime = moment(item.updatedAt).format("D.M.YYYY HH:mm");
+        const dateOfPost = postedTime.split(" ")[0];
+        const timeOfPost = postedTime.split(" ")[1];
+        return (
+          <div className="blog-items-wrapper" key={item.id}>
+            <div className="row no-gutters">
+              <div className="image-wrapper">
+                <img
+                  className="blog-item-avatar"
+                  src="https://i.picsum.photos/id/454/80/80.jpg?hmac=lHlKkUsxK0B6KymVYwr7iMd-VQeoP4XvY0IQKEJ4zXY"
+                  alt="Blog img"
+                />
+              </div>
+              <div className="blog-item-info-wrapper">
+                <h5 className="blog-item-title">{item.title}</h5>
+                <p className="blog-item-details">
+                  Posted date: {dateOfPost} at {timeOfPost} by Some person{" "}
+                  {index}
+                </p>
+              </div>
+              <div className="blog-item-btn-wrapper">
+                <button type="button" className="blog-item-btn edit-btn">
+                  Edit
+                </button>
+                <button
+                  type="button"
+                  className="blog-item-btn delete-btn"
+                  onClick={() => deletePost(item.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+            <div className="row no-gutters">
+              <p className="blog-post-content">{item.text}</p>
+            </div>
+            <div className="row no-gutters">
+              <div className="blog-post-images-wrapper">
+                <img
+                  src="https://i.picsum.photos/id/109/100/100.jpg?hmac=LeINYGZbnzwgFSznadxHjdAq1Mi13ARh3urcQf0H_LA"
+                  alt=""
+                  className="blog-post-image"
+                />
+                <img
+                  src="https://i.picsum.photos/id/195/100/100.jpg?hmac=vRu_NpzSS_L4oZCg-OfhAeRweDc_RHC5UccZxQ6rTLk"
+                  alt=""
+                  className="blog-post-image"
+                />
+                <img
+                  src="https://i.picsum.photos/id/140/100/100.jpg?hmac=4Skh7lTq0bdRmnXi_Pkq6odpzJruyj3UDBQWGHkCO-o"
+                  alt=""
+                  className="blog-post-image"
+                />
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,30 +4,30 @@ import "./navbar.scss";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-dark navbar-wrapper">
-      <a class="navbar-brand navbar-title" href="/">
+    <nav className="navbar navbar-dark navbar-wrapper">
+      <a className="navbar-brand navbar-title" href="/">
         My blog
       </a>
-      <form class="form-inline">
+      <form className="form-inline">
         <input
-          class="form-control mr-sm-2 navbar-search"
+          className="form-control mr-sm-2 navbar-search"
           type="search"
           placeholder="Search"
           aria-label="Search"
         />
-        <a class="nav-link" href="/">
+        <a className="nav-link" href="/">
           Link 1
         </a>
-        <a class="nav-link" href="/">
+        <a className="nav-link" href="/">
           Link 2
         </a>
-        <a class="nav-link" href="/">
+        <a className="nav-link" href="/">
           Link 3
         </a>
-        <a class="nav-link" href="/">
+        <a className="nav-link" href="/">
           My profile
         </a>
-        <a class="nav-link" href="/">
+        <a className="nav-link" href="/">
           Logout
         </a>
       </form>
